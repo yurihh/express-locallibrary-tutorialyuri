4ebf9e042884eb99ac8ab0aff520ad67645836cf
dani980boo <daniel980boo@gmail.com>
2023-09-12 10:42:18 -0300

    atividade pronta

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-const express = require("express");
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET home page. */
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: "You are so cool!!!!!" });
+});
+
+/* GET home page. */
+router.get('/ifc', function(req, res, next) {
+  res.render('index', { title: "Ifc é muito bom" });
+});
+
+/* GET home page. */
+router.get('/cool/beach', function(req, res, next) {
+  res.send("praia legal nao sei");
 });
 
 module.exports = router;
